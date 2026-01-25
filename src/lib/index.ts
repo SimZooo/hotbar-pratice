a226d7370158005e53be9a0ac3a4779bc23d4fd3
SimZooo <simenmathiesen@outlook.com>
2026-01-25 20:47:43 +0100

    wiP

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,23 +1,52 @@
 import { persisted, type Persisted } from "svelte-persisted-store";
+import { writable, type Writable } from "svelte/store";
 
 export enum Page {
     Main,
     Running,
     Options,
+    Tutorial,
 }
 
-export let settings: Persisted<object> = persisted("settings", {
-    "1": "1",
-    "2": "2",
-    "3": "3",
-    "4": "4",
-    "5": "5",
-    "6": "6",
-    "7": "7",
-    "8": "8",
-    "9": "9",
-    offhand: "f",
-});
+export enum Item {
+    None,
+    Pickaxe,
+    Axe,
+    Waterbucket,
+}
+
+export let hotbar: Writable<Item[]> = writable([
+    Item.None,
+    Item.None,
+    Item.None,
+    Item.None,
+    Item.None,
+    Item.None,
+    Item.None,
+    Item.None,
+    Item.None,
+]);
+
+export let default_settings = {
+    keybinds: {
+        "1": "1",
+        "2": "2",
+        "3": "3",
+        "4": "4",
+        "5": "5",
+        "6": "6",
+        "7": "7",
+        "8": "8",
+        "9": "9",
+        offhand: "f",
+    },
+    options: {
+        offhand_enabled: true,
+    },
+};
+
+export let settings: Persisted<{ keybinds: object; options: object }> =
+    persisted("settings", { ...default_settings });
 
 export let stats: Persisted<{ pb_cps: number }> = persisted("stats", {
     pb_cps: 0,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
-    import { Page, settings, stats } from "$lib";
+    import {
+        default_settings,
+        hotbar,
+        Item,
+        Page,
+        settings,
+        stats,
+    } from "$lib";
     import { onMount } from "svelte";
-
-    let new_slot = (): number => {
-        return Math.min(Math.ceil(Math.random() * 9), 8);
-    };
-
     let durations = [5, 10, 30, 60, 120];
 
+    let new_slot = (ignore: number[] | null): number => {
+        let r = Math.min(Math.ceil(Math.random() * 9), 8);
+        if (ignore !== null) {
+            while (ignore.includes(r)) {
+                ((r = Math.floor(Math.random() * 9)), 8);
+            }
+        }
+        return r;
+    };
+
     let slots: HTMLImageElement[] = $state([]);
-    let to_select = $state(new_slot());
+    let to_select = $state(new_slot(null));
     let start_time: number = $state(0);
     let duration: number = $state(durations[0]);
     let editing_keybind: string | null = $state(null);
@@ -23,6 +35,35 @@
 
     let container: HTMLDivElement;
     let selectedPos = $state({ left: 0, top: 0 });
+
+    // Check if settings are correct (not old version)
+    if ($settings.keybinds === undefined || $settings.options === undefined) {
+        settings.update((sets) => {
+            return default_settings;
+        });
+    }
+
+    function randomise_hotbar() {
+        let items = Object.values(Item).filter(
+            (item) => typeof item === "string" && item !== Item.None.toString(),
+        );
+
+        let ignore = [];
+
+        for (const _ in $hotbar) {
+            let i = Math.floor(Math.random() * items.length);
+            let rand_item = items[i] as Item;
+            items.splice(i, 1);
+
+            let j = new_slot(ignore);
+            ignore.push(j);
+            $hotbar[j] = rand_item;
+
+            if (items.length === 0) {
+                return;
+            }
+        }
+    }
 
     // Update selectedPos after the component mounts or when to_select changes
     function updateSelectedPos() {
@@ -44,7 +85,9 @@
     function start() {
         start_time = Date.now();
         current_page = Page.Running;
+        randomise_hotbar();
         interval_id = setInterval(() => {
+            now = Date.now();
             if (Date.now() - start_time >= duration * 1000) {
                 current_page = Page.Main;
                 start_time = 0;
@@ -61,8 +104,6 @@
                 let elapsed_s = (now - start_time) / 1000;
                 current_cps = current_clicks / elapsed_s;
             }
-
-            now = Date.now();
         }, 100);
     }
 
@@ -72,41 +113,40 @@
             updateSelectedPos();
         });
 
-        app?.addEventListener("click", () => (app?.focus(), { once: true }));
-
         history.pushState(null, "", location.href);
         window.addEventListener("popstate", () => {
             history.pushState(null, "", location.href);
         });
 
         app?.addEventListener("pointerdown", (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            e.stopImmediatePropagation();
             if (current_page === Page.Options && editing_keybind !== null) {
                 let button_label = "Mouse" + e.button;
                 settings.update((sets) => {
-                    if (!Object.values(sets).includes(button_label)) {
-                        sets[editing_keybind] = button_label;
+                    if (!Object.values(sets.keybinds).includes(button_label)) {
+                        sets.keybinds[editing_keybind] = button_label;
                     }
                     return sets;
                 });
                 editing_keybind = null;
             } else if (
                 current_page === Page.Running &&
-                "Mouse" + e.button === $settings[(to_select + 1).toString()]
+                "Mouse" + e.button ===
+                    $settings.keybinds[(to_select + 1).toString()]
             ) {
-                to_select = new_slot();
+                to_select = new_slot([to_select]);
                 updateSelectedPos();
                 current_clicks += 1;
             }
         });
 
-        app?.addEventListener("keydown", (e) => {
+        window.addEventListener("keydown", (e) => {
+            if (e.repeat) {
+                return;
+            }
             if (current_page === Page.Options && editing_keybind !== null) {
                 settings.update((sets) => {
-                    if (!Object.values(sets).includes(e.key)) {
-                        sets[editing_keybind] = e.key;
+                    if (!Object.values(sets.keybinds).includes(e.key)) {
+                        sets.keybinds[editing_keybind] = e.key;
                     }
                     return sets;
                 });
@@ -115,9 +155,9 @@
 
             if (
                 current_page === Page.Running &&
-                e.key === $settings[(to_select + 1).toString()]
+                e.key === $settings.keybinds[(to_select + 1).toString()]
             ) {
-                to_select = new_slot();
+                to_select = new_slot([to_select]);
                 updateSelectedPos();
                 current_clicks += 1;
             }
@@ -133,7 +173,6 @@
 </script>
 
 <div
-    tabindex="0"
     bind:this={app}
     class="relative w-screen h-screen overflow-hidden"
     bind:this={container}
@@ -151,11 +190,23 @@
         >
 
         <div
-            class="absolute w-1/3 place-self-center top-10 h-full text-white flex flex-col gap-30 text-xl"
+            class="absolute w-1/3 place-self-center top-10 h-full text-white flex flex-col gap-30 text-xl min-h-0 overflow-y-auto"
         >
-            <p class=" text-4xl">Options</p>
-            <div class="flex-col grid grid-cols-2 grid-rows-10 gap-5">
-                {#each Object.keys($settings) as setting}
+            <p class="text-4xl">Options</p>
+            <div class="grid grid-cols-2 align-middle">
+                <p class="text-start">Show Offhand Slot</p>
+                <button
+                    class="bg-[#707070] p-2 w-50 border border-black outline-none text-center hover:cursor-pointer"
+                    onclick={() =>
+                        ($settings.options.offhand_enabled =
+                            !$settings.options.offhand_enabled)}
+                >
+                    {$settings.options.offhand_enabled}
+                </button>
+            </div>
+            <p class="text-4xl">Keybinds</p>
+            <div class="grid grid-cols-2 grid-rows-auto gap-5 items-center">
+                {#each Object.keys($settings.keybinds) as setting}
                     <p class="text-start">Hotbar Slot {setting}</p>
                     <button
                         class="bg-[#707070] p-2 w-50 border border-black outline-none text-center hover:cursor-pointer"
@@ -164,7 +215,7 @@
                         {#if editing_keybind === setting}
                             _
                         {:else}
-                            {$settings[setting]}
+                            {$settings.keybinds[setting]}
                         {/if}
                     </button>
                 {/each}
@@ -188,21 +239,32 @@
                     Time: {Math.floor((now - start_time) / 1000) + 1}
                 </p>
             </div>
-            <div class="flex justify-center items-center size-full gap-3">
-                <img
-                    src="./offhand.png"
-                    alt=""
-                    class="h-42 aspect-square shadow-2xl shadow-black place-self-center"
-                />
+            <div class="flex justify-center items-center size-full gap-12">
+                {#if $settings.options.offhand_enabled}
+                    <img
+                        src="./offhand.png"
+                        alt=""
+                        class="h-32 aspect-square shadow-2xl shadow-black place-self-center"
+                    />
+                {/if}
                 <div class="flex">
                     {#each Array(9) as _, i}
-                        <img
-                            src="./slot.png"
-                            alt=""
-                            class="h-40 aspect-square shadow-2xl shadow-black"
-                            bind:this={slots[i]}
-                            onload={updateSelectedPos}
-                        />
+                        <div class="relative h-30 aspect-square">
+                            <img
+                                src="./slot.png"
+                                alt=""
+                                class="h-full w-full shadow-2xl shadow-black"
+                                bind:this={slots[i]}
+                                onload={updateSelectedPos}
+                            />
+
+                            <p
+                                class="absolute inset-0 flex items-center justify-center
+                                       text-white text-xl pointer-events-none z-10"
+                            >
+                                {$hotbar[i]}
+                            </p>
+                        </div>
                     {/each}
                 </div>
             </div>
@@ -210,13 +272,31 @@
             <img
                 src="./selected.png"
                 alt=""
-                class="h-42 aspect-square absolute"
+                class="h-32 aspect-square absolute"
                 style="left: {selectedPos.left}px; top: {selectedPos.top - 4}px"
             />
-
+        </div>
+        <button
+            class="left-25 top-5 absolute text-white text-2xl w-60 h-15 text-center items-center bg-[#6F6F6F] flex flex-col justify-center border-3 border-black hover:cursor-pointer text-shadow-black text-shadow-lg z-1000"
+            onclick={() => {
+                current_page = Page.Main;
+                reset();
+            }}>Back</button
+        >
+    {:else if current_page === Page.Tutorial}
+        <div
+            class="relative flex flex-col size-full justify-center z-20 text-white text-center text-3xl"
+        >
+            <p>
+                Press the hotbar keybind corresponding to the highlighted slot.
+            </p>
+            <p>You can edit your preferred keybinds in the options!</p>
             <button
-                class="right-25 top-5 absolute text-white text-2xl w-60 h-15 text-center items-center bg-[#6F6F6F] flex flex-col justify-center border-3 border-black hover:cursor-pointer text-shadow-black text-shadow-lg"
-                onclick={() => (current_page = Page.Options)}>Options</button
+                class="left-25 top-5 absolute text-white text-2xl w-60 h-15 text-center items-center bg-[#6F6F6F] flex flex-col justify-center border-3 border-black hover:cursor-pointer text-shadow-black text-shadow-lg z-1000"
+                onclick={() => {
+                    current_page = Page.Main;
+                    reset();
+                }}>Back</button
             >
         </div>
     {:else}
@@ -255,11 +335,32 @@
             {/each}
         </div>
 
-        <button
-            class="right-25 top-5 absolute text-white text-2xl w-60 h-15 text-center items-center bg-[#6F6F6F] flex flex-col justify-center border-3 border-black hover:cursor-pointer text-shadow-black text-shadow-lg"
-            onclick={() => (current_page = Page.Options)}>Options</button
+        <div
+            class="absolute right-25 top-5 flex flex-col w-fit justify-center gap-5"
         >
+            <button
+                class="text-white text-2xl w-60 h-15 text-center items-center bg-[#6F6F6F] flex flex-col justify-center border-3 border-black hover:cursor-pointer text-shadow-black text-shadow-lg"
+                onclick={() => (current_page = Page.Options)}>Options</button
+            >
+            <button
+                class="text-white text-2xl w-60 h-15 text-center items-center bg-[#6F6F6F] flex flex-col justify-center border-3 border-black hover:cursor-pointer text-shadow-black text-shadow-lg"
+                onclick={() => (current_page = Page.Tutorial)}
+                >How to Play</button
+            >
+        </div>
     {/if}
+    <a
+        href="https://buymeacoffee.com/simzooo"
+        target="_black"
+        class="absolute bottom-10 left-10 bg-[#FFDD00] flex flex-row gap-2 rounded items-center p-2"
+    >
+        <img
+            src="https://cdn.buymeacoffee.com/widget/assets/coffee%20cup.svg"
+            alt="Buy Me A Coffee"
+            style="height: 36px; width: 36px; margin: 0; padding: 0;"
+        />
+        Support me!
+    </a>
 </div>
 
 <style>
